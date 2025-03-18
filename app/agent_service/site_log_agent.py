0449d4ab4c2075ffadfa61492534dc0202e4d86d
DEBASMITROY2002 <ddruk2018@gmail.com>
2025-03-18 18:45:37 +0530

    port fix

--- app/agent_service/site_log_agent.py
+++ app/agent_service/site_log_agent.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/debasmitroy/Desktop/programming/gemini-agent-assist/key.json"
-os.environ["GOOGLE_CLOUD_PROJECT"] = "hackathon0-project"
+os.environ["GOOGLE_CLOUD_PROJECT"] = "openserve-0"
 os.environ["GOOGLE_CLOUD_LOCATION"] = "us-central1"
 
 
